4eb86e5c0d93827e2e1018adf797a7e7eea5a2a5
Philip Geurin <philip.geurin@gmail.com>
2024-06-09 23:59:04 -0700

    copy in example

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
+# import gradio as gr
+
+# def greet(name):
+#     return "Hello " + name + "!!"
+
+# demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+# demo.launch()
+
+from fastai.vision.all import *
 import gradio as gr
 
-def greet(name):
-    return "Hello " + name + "!!"
+def is_cat(x): return x[0].isupper()
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
-demo.launch()
+learn = load_learner('cats-dogs-model.pkl')
+
+categories = ('Dog', 'Cat')
+
+def classify_image(img):
+    pred,idx,probs = learn.predict(img)
+    return dict(zip(categories, map(float,probs)))
+
+image = gr.inputs.Image(shape=(192, 192))
+label = gr.outputs.Label()
+examples = ['dog.jpg', 'cat.jpg', 'dunno.jpg']
+
+intf = gr.Interface(fn=classify_image, inputs=image, outputs=label, examples=examples)
+intf.launch(inline=False)
